efecb50fb9cc4d85c8b7dde90a805b4d46d6cf69
e.finogeev <finny88@mail.ru>
2026-02-11 18:53:54 +0300

    add mantine

--- apps/portal/src/app/App.tsx
+++ apps/portal/src/app/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import ReactLogo from '@shared/assets/react.svg?react'
 import viteLogo from '/vite.svg'
+import '@mantine/core/styles.css'
 import './App.css'
+
+import {
+	Button,
+	MantineProvider
+} from '@mantine/core'
+import { theme } from './theme.ts'
 
 const App = () => {
 	const [count, setCount] = useState(0)
 
 	return (
-		<>
+		<MantineProvider theme={theme}>
 			<div>
 				<a href={'https://vite.dev'} target={'_blank'}>
 					<img src={viteLogo} className={'logo'} alt={'Vite logo'} />
@@ -19,9 +26,9 @@ const App = () => {
 			</div>
 			<h1>Vite + React</h1>
 			<div className={'card'}>
-				<button onClick={() => setCount(prev => prev + 1)}>
+				<Button variant={'filled'} onClick={() => setCount(prev => prev + 1)}>
 					count is {count}
-				</button>
+				</Button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
@@ -29,7 +36,7 @@ const App = () => {
 			<p className={'read-the-docs'}>
 				Click on the Vite and React logos to learn more
 			</p>
-		</>
+		</MantineProvider>
 	)
 }
 
